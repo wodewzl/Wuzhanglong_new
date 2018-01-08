14991e50e3921f43a36423250bdc99aed7c7f272
wuzhanglong <273415345@qq.com>
2018-01-09 00:37:13 +0800

    提交代码

--- app/src/main/java/com/wzl/feifubao/activity/ShopDetailActivity.java
+++ app/src/main/java/com/wzl/feifubao/activity/ShopDetailActivity.java
@@ -3,6 +3,10 @@ package com.wzl.feifubao.activity;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +15,8 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.github.jdsjlzx.recyclerview.LuRecyclerViewAdapter;
+import com.github.jdsjlzx.recyclerview.ProgressStyle;
 import com.rey.material.app.BottomSheetDialog;
 import com.rey.material.widget.Button;
 import com.rey.material.widget.CheckBox;
@@ -22,26 +28,33 @@ import com.wuzhanglong.library.utils.BaseCommonUtils;
 import com.wuzhanglong.library.utils.FileUtil;
 import com.wuzhanglong.library.utils.WebviewUtil;
 import com.wzl.feifubao.R;
+import com.wzl.feifubao.adapter.LifeAdapter;
+import com.wzl.feifubao.adapter.ShopChoseAdapter;
 import com.wzl.feifubao.application.AppApplication;
 import com.wzl.feifubao.constant.Constant;
 import com.wzl.feifubao.mode.RateQueryVO;
 import com.wzl.feifubao.mode.ShopDetailVO;
+import com.wzl.feifubao.mode.ShopHomeVO;
 import com.wzl.feifubao.mode.ShopVO;
 import com.youth.banner.Banner;
 import com.youth.banner.BannerConfig;
 import com.youth.banner.loader.ImageLoader;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import me.zhanghai.android.materialratingbar.MaterialRatingBar;
 
-public class ShopDetailActivity extends BaseActivity {
+public class ShopDetailActivity extends BaseActivity implements View.OnClickListener {
     private BottomSheetDialog mDialog;
     private Banner mBanner;
-    private TextView mNameTv,mPriceTv,mCommentTv,mCommentCountTv;
+    private TextView mNameTv, mPriceTv, mCommentTv, mCommentCountTv;
     private MaterialRatingBar mRatingBar;
     private WebView mWebView;
+    private List<ShopDetailVO.DataBean.SpecListBean.ValueBean> mList = new ArrayList<>();
+    private TextView mAddCartTv, mBuyTv;
 
     @Override
     public void baseSetContentView() {
@@ -53,18 +66,20 @@ public class ShopDetailActivity extends BaseActivity {
         mBanner = getViewById(R.id.banner);
         mBanner.setBannerStyle(BannerConfig.NUM_INDICATOR);
         mBanner.setIndicatorGravity(BannerConfig.RIGHT);
-        mNameTv=getViewById(R.id.name_tv);
-        mPriceTv=getViewById(R.id.price_tv);
+        mNameTv = getViewById(R.id.name_tv);
+        mPriceTv = getViewById(R.id.price_tv);
         mRatingBar = getViewById(R.id.rating_bar);
-        mWebView=getViewById(R.id.webView);
+        mWebView = getViewById(R.id.webView);
         WebviewUtil.SetWebview(mWebView);
-        mCommentTv=getViewById(R.id.comment_tv);
-        mCommentCountTv=getViewById(R.id.comment_count_tv);
-
+        mCommentTv = getViewById(R.id.comment_tv);
+        mCommentCountTv = getViewById(R.id.comment_count_tv);
+        mAddCartTv = getViewById(R.id.add_cart_tv);
+        mBuyTv = getViewById(R.id.buy_tv);
     }
 
     @Override
     public void bindViewsListener() {
+        mAddCartTv.setOnClickListener(this);
     }
 
     @Override
@@ -75,14 +90,14 @@ public class ShopDetailActivity extends BaseActivity {
             map.put("uid", AppApplication.getInstance().getUserInfoVO().getData().getUid());
 //        map.put("goodsId", this.getIntent().getStringExtra("id"));
         map.put("goodsId", "3");
-        HttpGetDataUtil.get(mActivity, this, Constant.SHOP_DETAIL_ULR, map,ShopDetailVO.class);
+        HttpGetDataUtil.get(mActivity, this, Constant.SHOP_DETAIL_ULR, map, ShopDetailVO.class);
     }
 
     @Override
     public void hasData(BaseVO vo) {
         ShopDetailVO shopDetailVO = (ShopDetailVO) vo;
-        ShopDetailVO.DataBean dataBean=shopDetailVO.getData();
-        if (dataBean.getImg_list() != null && dataBean.getImg_list().size()> 0) {
+        ShopDetailVO.DataBean dataBean = shopDetailVO.getData();
+        if (dataBean.getImg_list() != null && dataBean.getImg_list().size() > 0) {
             mBanner.setImages(dataBean.getImg_list());
 
             mBanner.setImageLoader(new ImageLoader() {
@@ -96,12 +111,22 @@ public class ShopDetailActivity extends BaseActivity {
         }
 
         mNameTv.setText(dataBean.getGoods_name());
-        mPriceTv.setText(dataBean.getPrice()+"P");
+        mPriceTv.setText(dataBean.getPrice() + "P");
         mRatingBar.setEnabled(false);
         mRatingBar.setRating(BaseCommonUtils.parseInt(dataBean.getMatch_point()));
-        mWebView.loadDataWithBaseURL(FileUtil.getSaveFilePath(this,Constant.SDCARD_CACHE), dataBean.getDescription(), "text/html", "utf-8", null);
+        mWebView.loadDataWithBaseURL(FileUtil.getSaveFilePath(this, Constant.SDCARD_CACHE), dataBean.getDescription(), "text/html", "utf-8", null);
         mCommentTv.setText(dataBean.getMatch_point());
-        mCommentCountTv.setText(dataBean.getEvaluates_count()+"人已评价");
+        mCommentCountTv.setText(dataBean.getEvaluates_count() + "人已评价");
+
+        for (int i = 0; i < dataBean.getSpec_list().size(); i++) {
+            ShopDetailVO.DataBean.SpecListBean.ValueBean valueBean = new ShopDetailVO.DataBean.SpecListBean.ValueBean();
+            valueBean.setSpec_id(dataBean.getSpec_list().get(i).getSpec_id());
+            valueBean.setSpec_name(dataBean.getSpec_list().get(i).getSpec_name());
+            mList.add(valueBean);
+            for (int j = 0; j < dataBean.getSpec_list().get(i).getValue().size(); j++) {
+                mList.add(dataBean.getSpec_list().get(i).getValue().get(j));
+            }
+        }
     }
 
     @Override
@@ -114,13 +139,44 @@ public class ShopDetailActivity extends BaseActivity {
 
     }
 
-    public void choseShop(){
+    public void choseShop() {
+        View dialogView = View.inflate(this, R.layout.shop_chose_dialog, null);
+
         mDialog = new BottomSheetDialog(mActivity);
-        View dialogView = View.inflate(mActivity, R.layout.pay_view, null);
+            RecyclerView recyclerView = dialogView.findViewById(R.id.recycler_view);
+            GridLayoutManager layoutManager=new GridLayoutManager(this,5);
+            recyclerView.setLayoutManager(layoutManager);
+            final ShopChoseAdapter adapter = new ShopChoseAdapter(recyclerView);
+        recyclerView.setAdapter(adapter);
+            adapter.updateData(mList);
+
+        layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                ShopDetailVO.DataBean.SpecListBean.ValueBean vo= (ShopDetailVO.DataBean.SpecListBean.ValueBean) adapter.getData().get(position);
+                if(TextUtils.isEmpty(vo.getSpec_value_name())){
+                    return 5;
+                }else {
+                    return 1;
+                }
+            }
+        });
         mDialog.contentView(dialogView)
                 .heightParam(ViewGroup.LayoutParams.WRAP_CONTENT)
                 .inDuration(500)
                 .cancelable(true)
                 .show();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_cart_tv:
+                choseShop();
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/wzl/feifubao/adapter/ShopChoseAdapter.java
@@ -0,0 +1,43 @@
+package com.wzl.feifubao.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+import com.wuzhanglong.library.adapter.RecyclerBaseAdapter;
+import com.wzl.feifubao.R;
+import com.wzl.feifubao.mode.ShopDetailVO;
+import com.wzl.feifubao.mode.ShopVO;
+
+import cn.bingoogolapple.baseadapter.BGAViewHolderHelper;
+
+/**
+ * Created by wuzhanglong on 2018/1/1.
+ */
+
+public class ShopChoseAdapter extends RecyclerBaseAdapter<ShopDetailVO.DataBean.SpecListBean.ValueBean> {
+    public ShopChoseAdapter(RecyclerView recyclerView) {
+        super(recyclerView, R.layout.shop_chose_adapter_type1);
+    }
+
+    @Override
+    public void initData(BGAViewHolderHelper helper, int position, Object model) {
+        ShopDetailVO.DataBean.SpecListBean.ValueBean vo= (ShopDetailVO.DataBean.SpecListBean.ValueBean) model;
+        if(getItemViewType( position)==R.layout.shop_chose_adapter_type1){
+            helper.setText(R.id.title_tv,vo.getSpec_name());
+        }else{
+            helper.setText(R.id.value_tv,vo.getSpec_value_name());
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        ShopDetailVO.DataBean.SpecListBean.ValueBean valueBean= (ShopDetailVO.DataBean.SpecListBean.ValueBean) this.getData().get(position);
+        if(TextUtils.isEmpty(valueBean.getSpec_value_name())){
+            return R.layout.shop_chose_adapter_type1;
+        }else {
+            return R.layout.shop_chose_adapter_type2;
+        }
+    }
+}
