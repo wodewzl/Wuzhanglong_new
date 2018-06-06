a1ace96bfddc36ea992c42e38d7158865508ba19
wodewzl <944908457@qq.com>
2018-06-06 15:47:42 +0800

    ddd

--- caipiao/src/main/java/com/wzl/caipiao/MainActivity.java
+++ caipiao/src/main/java/com/wzl/caipiao/MainActivity.java
@@ -17,6 +17,7 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
@@ -50,7 +51,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         if (AppApplication.getInstance().getDataVO() != null) {
             List<UserInfoVO> list = AppApplication.getInstance().getDataVO();
             mMainAdapter.updateDataFrist(list);
-            countData(mMainAdapter.getData());
+//            countData(mMainAdapter.getData());
         }
     }
 
@@ -85,7 +86,18 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.commit_tv:
-                String reslut = mNumberEt.getText().toString();
+                StringBuffer sb = new StringBuffer(mNumberEt.getText().toString());
+                sb.reverse();
+                String reslut = sb.toString();
+
+//                StringBuffer sb = new StringBuffer();
+//                for (int i = 0; i < 150; i++) {
+//                    Random random=new Random();
+//                    String str= random.nextInt(10)+"";
+//                    sb.append(str);
+//                }
+//                String reslut = sb.toString();
+
                 if (TextUtils.isEmpty(reslut)) {
                     showCustomToast("请输入开奖结果");
                     return;
@@ -94,15 +106,13 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 for (int i = 0; i < reslut.length(); i++) {
                     UserInfoVO userInfoVO = new UserInfoVO();
                     userInfoVO.setResult(String.valueOf(reslut.charAt(i)));
-//                    userInfoVO.setYuce1(mTv3.getText().toString());
-//                    userInfoVO.setYuce2(mTv4.getText().toString());
                     list.add(userInfoVO);
                 }
                 mMainAdapter.updateDataFrist(list);
-                countData(mMainAdapter.getData());
+                countData(list);
                 mNumberEt.setText("");
                 break;
-            case R.id.ok_tv:
+            case R.id.base_ok_tv:
                 new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
                         .setTitleText("确定要保存数据吗吗?")
                         .setConfirmText("确定")
@@ -148,42 +158,56 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         if (list == null)
             return;
 
-        StringBuffer sb1 = new StringBuffer();
-        StringBuffer sb2 = new StringBuffer();
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
-        for (int i = 0; i < list.size(); i++) {
-            //统计双数
-            if (sb1.toString().contains(list.get(i).getResult())) {
-                if (!sb2.toString().contains(list.get(i).getResult())) {
-                    sb2.append(list.get(i).getResult());
+
+        if (mMainAdapter.getData().size() > 10) {
+
+            for (int j = 0; j < list.size(); j++) {
+                StringBuffer sb1 = new StringBuffer();
+                StringBuffer sb2 = new StringBuffer();
+                HashMap<String, Integer> map = new HashMap<String, Integer>();
+                for (int i = j; i < mMainAdapter.getData().size(); i++) {
+                    //统计双数
+                    if (sb1.toString().contains(((UserInfoVO) mMainAdapter.getData().get(i)).getResult())) {
+                        if (!sb2.toString().contains(((UserInfoVO) mMainAdapter.getData().get(i)).getResult())) {
+                            sb2.append(((UserInfoVO) mMainAdapter.getData().get(i)).getResult());
+                        }
+                    } else {
+                        sb1.append(((UserInfoVO) mMainAdapter.getData().get(i)).getResult());
+                    }
+                    String tv1Str = sb2.toString();
+                    mTv1.setText(tv1Str);
+
+
+                    //统计数量
+                    if (mMainAdapter.getData().size() > 120 && i < 120) {
+                        String key = ((UserInfoVO) mMainAdapter.getData().get(i)).getResult();
+                        Integer count = map.get(key);
+                        if (count == null) {
+                            count = 1;
+                        } else {
+                            count++;
+                        }
+                        map.put(key, count);
+                    }
                 }
-            } else {
-                sb1.append(list.get(i).getResult());
-            }
-            String tv1Str = sb2.toString();
-            mTv1.setText(tv1Str);
-            if (sb2.toString().length() > 1) {
-                mTv3.setText(sb2.toString().substring(0, 2));
-                list.get(i).setYuce1(mTv3.getText().toString());
+
+                if (sb2.toString().length() > 1) {
+                    mTv3.setText(sb2.toString().substring(0, 2));
+                    ((UserInfoVO) mMainAdapter.getData().get(j)).setYuce1(mTv3.getText().toString());
+                }
+
+                if (mMainAdapter.getData().size() > 120) {
+                    String tv2Str = sortMapByValue(map);
+                    mTv2.setText(tv2Str);
+                    if (tv2Str.length() > 1) {
+                        mTv4.setText(tv2Str.substring(0, 2));
+                    }
+                }
+
+                mMainAdapter.notifyDataSetChanged();
             }
 
-            //统计数量
-            if (list.size() > 120) {
-                String key = list.get(i).getResult();
-                Integer count = map.get(key);
-                if (count == null) {
-                    count = 1;
-                } else {
-                    count++;
-                }
-                map.put(key, count);
-                String tv2Str = sortMapByValue(map);
-                mTv2.setText(tv2Str);
-                if (tv2Str.length() > 1) {
-                    mTv4.setText(tv2Str.substring(0, 2));
-                }
-            }
+
         }
-        mMainAdapter.notifyDataSetChanged();
     }
 }
